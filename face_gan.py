3b7e19e4a60e1d51462dab2439335bd132290e7c
Brandon Benton <brandon.benton.5255@gmail.com>
2025-07-05 18:02:50 -0500

    define inputs

--- face_gan.py
+++ face_gan.py
@@ -20,3 +20,18 @@ print("Random Seed:", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 torch.use_deterministic_algorithms(True)
+
+#Defining inputs
+dataroot = "data/celeba"
+workers = 2
+batch_size = 128
+image_size = 64
+nc = 3
+nz = 100
+ngf = 64
+ndf = 64
+num_epochs = 5
+lr = 0.0002
+beta1 = 0.5
+ngpu = 1
+
