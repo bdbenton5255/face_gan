41ffa455709642af90f4fb02d13806bef132e00b
Brandon Benton <brandon.benton.5255@gmail.com>
2025-07-05 18:59:54 -0500

    plot training data

--- face_gan.py
+++ face_gan.py
@@ -48,5 +48,13 @@ dataset = dset.ImageFolder(root=dataroot,
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
 
 #Configure GPU or CPU
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+device = torch.device("cpu")
 print(device)
+
+#Plot training data
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.show()
