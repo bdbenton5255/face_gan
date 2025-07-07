b520eb0f186fa36e5ac2cf6798d1723350762c08
Brandon Benton <brandon.benton.5255@gmail.com>
2025-07-06 23:49:32 -0500

    plot loss and visualize generator regression

--- face_gan.py
+++ face_gan.py
@@ -231,3 +231,21 @@ for epoch in range(num_epochs):
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
         
         iters += 1
+
+#Plot loss during training
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+#Visualize generator progression
+fig = plt.figure(figsize(8,8))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+HTML(ani.to_jshtml())
